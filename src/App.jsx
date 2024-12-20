32b89d176e28edd24f20b1db9b5d8428c4ac3f59
githltech <hltechindiaprivatelimited@gmail.com>
2024-12-20 15:14:17 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,6 @@ import React,{useEffect} from 'react';
 import './App.css';
 import './index.css';
 import { BrowserRouter as Router , Routes , Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './registration.jsx/Login';
-import RVRentalForm from './pages/RVRentalForm';
-import Contactus from './pages/Contactus';
-import BookingPage from './pages/BookingPage';
-import WhyChooseUs from './pages/WhyChooseUs';
-import AboutUsPage from './pages/AboutUsPage';
-import Dpharma from './pages/courses/Dpharma';
-import Bpharma from './pages/courses/Bpharma';
-import TrnAndPlc from './pages/Placement/TrnAndPlc';
-import OurTeam from './pages/courses/OurTeam';
-import FacultyDetails from "./components/FacultyDetails";
-import Gallery from "./components/Gallary";
-
 
 
 
@@ -36,20 +22,10 @@ const  App = ()  =>{
 
   return (
     <>
-    <Router basename="/DBSRCollege"> // 
+    <Router basename="/DBSRCollege">
       <Routes>
-        <Route path='aboutus' element = {<AboutUsPage/>}/>
-        <Route path='/rvrentalform' element = {<RVRentalForm/>}/>
-        <Route path='/contactus' element = {<Contactus/>}/>
-        <Route path="/booking" element = {<BookingPage/>}/>
-        <Route path='/login' element = {<Login/>}/>
-        <Route path='/whychooseus' element = {<WhyChooseUs/>}/>
-        <Route path='/dpharma' element = {<Dpharma/>} />
-        <Route path='/bpharma' element = {<Bpharma/>}/>
-        <Route path= '/trnandplc' element = {<TrnAndPlc/> }/>
-        <Route path='/ourteam' element = {<OurTeam/> } />
-        <Route path="/faculty" element={<FacultyDetails />} />
-        <Route path="/gallery" element={<Gallery />} />
+      <Route path="/" element={<Home />} />
+       
 
 
       </Routes>
